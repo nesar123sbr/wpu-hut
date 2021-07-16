4b7d34ff475fa47e9ed7848d7bfd99bd1465e706
Gada Prima <76141058+nesar123sbr@users.noreply.github.com>
2021-07-16 07:43:18 +0700

    first commit

---
+++ json/latihan1.php
@@ -0,0 +1,20 @@
+<?php 
+
+$developer = [
+    [
+        "nama" => "Gada Prima",
+        "nik"  => 1271010112000004,
+        "email" => "nesar123siburian@gmail.com"
+    ],
+    [
+        "nama" => "Dia",
+        "nik"  => 1271010112000004,
+        "email" => "nesarajasiburian@gmail.com"
+    ]    
+];
+
+$data = json_encode($developer);
+echo $data;
+
+
+?>
